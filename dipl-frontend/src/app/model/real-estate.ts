627f3c5bddc9f5da24077676939a3a6871c8812b
savapejovic95 <sava.pejovic@yahoo.com>
2020-03-03 22:47:07 +0100

    Added model RealEstate class FE

---
+++ dipl-frontend/src/app/model/real-estate.ts
@@ -0,0 +1,14 @@
+export class RealEstate {
+    id: string;
+    name: string;
+    price: string;
+    squareMeters: string;
+    type: string;
+    service: string;
+    city: string;
+    cityPart: string;
+    heating: string;
+    floor: string;
+    description: string;
+    additionalStuff: string;
+}
