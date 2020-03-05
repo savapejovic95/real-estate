4abc7e95efbc923f2a0ee0bcddfc7af5bf0b0896
savapejovic95 <sava.pejovic@yahoo.com>
2020-03-06 00:34:55 +0100

    Added logic for disabilng city parts and refactor

--- dipl-frontend/src/app/real-estate/real-estate-form/real-estate-form.component.ts
+++ dipl-frontend/src/app/real-estate/real-estate-form/real-estate-form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { RealEstate } from 'src/app/model/real-estate';
 import { Router } from '@angular/router';
 import { RealEstateService } from 'src/app/service/real-estates.service';
+import { City } from 'src/app/model/city';
+import { CityPart } from 'src/app/model/city-part';
 
 @Component({
   selector: 'app-real-estate-form',
@@ -12,6 +14,7 @@ export class RealEstateFormComponent implements OnInit {
 
   realEstate: RealEstate;
   city: string;
+  partsDisabled: boolean;
 
   constructor(
     private router: Router,
@@ -20,27 +23,30 @@ export class RealEstateFormComponent implements OnInit {
     this.realEstate = new RealEstate();
   }
 
-  public cities: Array<string> = [];
-  public cityParts: Array<string> = [];
+  public cities: Array<City> = [];
+  public cityParts: Array<CityPart> = [];
 
   ngOnInit(): void {
     this.citiesDropdownRefresh();
+    this.partsDisabled = true;
   }
 
   citiesDropdownRefresh() {
     this.realEstateService.findAllCities().subscribe(data=>{
       console.log(data);
       data.forEach(element => {
-        this.cities.push(element["name"])
+        this.cities.push(element)
       });
     })
   }
 
-  cityPartsDropdownRefresh() {
+  onCityChange() {
+    this.partsDisabled = false;
+    this.cityParts = [];
     this.realEstateService.findAllCityParts(this.city).subscribe(data=>{
       console.log(data);
       data.forEach(element => {
-        this.cityParts.push(element["name"])
+        this.cityParts.push(element)
       });
     })
   }
