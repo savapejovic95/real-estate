0b2664990245c6cc8c1f1262839185a729d34893
savapejovic95 <sava.pejovic@yahoo.com>
2020-03-09 22:40:49 +0100

    Sort design - initial

--- dipl-frontend/src/app/app.component.ts
+++ dipl-frontend/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CityPart } from './model/city-part';
+import { RealEstateService } from './service/real-estates.service';
+import { City } from './model/city';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +10,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string;
+  typeFilter: string;
+  serviceFilter: string;
+  cityIdFilter: string;
+  cityPartIdFilter: string;
+  partsDisabled: boolean;
+  priceFilter: string;
+  squareMetersFilter: string;
+  roomsFilter: string;
  
-  constructor() {
+  constructor(private realEstateService: RealEstateService) {
     this.title = 'SPIS - Agencija za nekretnine';
+  }
+
+  public cityParts: Array<CityPart> = [];
+  public cities: Array<City> = [];
+  public types: Array<string> = ["Stan", "Kuca", "Soba", "Garaza"];
+  public services: Array<string> = ["Prodaja", "Izdavanje"];
+
+  ngOnInit(): void {
+    this.citiesDropdownRefresh();
+    this.partsDisabled = true;
+  }
+
+  citiesDropdownRefresh() {
+    this.realEstateService.findAllCities().subscribe(data=>{
+      console.log(data);
+      data.forEach(element => {
+        this.cities.push(element)
+      });
+    })
+  }
+
+  onCityChange() {
+    this.partsDisabled = false;
+    this.cityParts = [];
+    this.realEstateService.findCityPartsByCityId(this.cityIdFilter).subscribe(data=>{
+      console.log(data);
+      data.forEach(element => {
+        this.cityParts.push(element)
+      });
+    })
+  }
+
+  onSubmit() {
+    //find with filters
   }
 }
