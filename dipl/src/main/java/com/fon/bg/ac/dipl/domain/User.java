74c55dd7453a973ceff397dcd9eedc7bbb70c615
Sava Pejovic <spejovic@s4hc.com>
2020-07-27 22:53:09 +0200

    Bug fix - saving real estate

--- dipl/src/main/java/com/fon/bg/ac/dipl/domain/User.java
+++ dipl/src/main/java/com/fon/bg/ac/dipl/domain/User.java
@@ -17,7 +17,7 @@ import javax.validation.constraints.Size;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @NotBlank
     @Size(max = 20)
@@ -47,11 +47,11 @@ public class User {
         this.password = password;
     }
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
